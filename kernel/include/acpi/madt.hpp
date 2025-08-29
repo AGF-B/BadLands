a01f6c6b64cfe72aaf1124a51afc1097340964b2
AProgrammablePhoenix <alexgf27550@gmail.com>
2025-08-29 20:55:26 +0200

    Add support for interrupt source overrides

--- kernel/include/acpi/madt.hpp
+++ kernel/include/acpi/madt.hpp
@@ -29,6 +29,16 @@ namespace ACPI {
             uint32_t GlobalSystemInterruptBase;
         };
 
+        struct InterruptSourceOverride {
+            static constexpr uint8_t _Type = 0x02;
+            uint8_t Type;
+            uint8_t Length;
+            uint8_t Bus;
+            uint8_t Source;
+            uint32_t GlobalSystemInterrupt;
+            uint32_t Flags;
+        };
+
         struct APICOverride {
             static constexpr uint8_t _Type = 0x05;
             uint8_t Type;
