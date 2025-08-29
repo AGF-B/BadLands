77939b2df57a9e1115832f6a827e06bfd02c3a2e
AProgrammablePhoenix <alexgf27550@gmail.com>
2025-08-29 20:58:21 +0200

    Update kernel entry to (now) show basic keyboard input info

--- kernel/src/entry.cpp
+++ kernel/src/entry.cpp
@@ -3,17 +3,21 @@
 #include <shared/efi/efi.h>
 #include <shared/memory/layout.hpp>
 
+#include <acpi/Interface.hpp>
+
+#include <devices/PS2/Controller.hpp>
+#include <devices/PS2/Keyboard.hpp>
+
 #include <fs/VFS.hpp>
 
-#include <GRM/ResourceManager.hpp>
-
+#include <interrupts/APIC.hpp>
 #include <interrupts/idt.hpp>
 #include <interrupts/Panic.hpp>
+#include <interrupts/RuntimeSvc.hpp>
 
 #include <mm/gdt.hpp>
 #include <mm/Heap.hpp>
 #include <mm/PhysicalMemory.hpp>
-#include <mm/Utils.hpp>
 #include <mm/VirtualMemory.hpp>
 
 #include <screen/Log.hpp>
@@ -24,8 +28,6 @@ extern uint8_t kernel_init_array_start[];
 extern uint8_t kernel_init_array_end[];
 
 namespace {
-    static const EFI_RUNTIME_SERVICES* rtServices;
-
     static void _kernel_ctx_init() {
         const size_t initialize_count = (
             kernel_init_array_end - kernel_init_array_start
@@ -37,32 +39,30 @@ namespace {
             )[i]();
         }
 
-        rtServices = reinterpret_cast<EFI_RUNTIME_SERVICES*>(
-            Shared::Memory::Layout::OsLoaderData.start + Shared::Memory::Layout::OsLoaderDataOffsets.RTServices
-        );
+        Runtime::Initialize();
     }
 
     static inline void SetupPhysicalMemory() {
         auto status = PhysicalMemory::Setup();
         if (status != PhysicalMemory::StatusCode::SUCCESS) {
             if (status == PhysicalMemory::StatusCode::OUT_OF_MEMORY) {
-                Panic::PanicShutdown(rtServices, "PMM INITIALIZATION FAILED (OUT OF MEMORY)\n\r");
+                Panic::PanicShutdown("PMM INITIALIZATION FAILED (OUT OF MEMORY)\n\r");
             }
             else {
-                Panic::PanicShutdown(rtServices, "PMM INITIALIZATION FAILED (UNKNOWN REASON)\n\r");
+                Panic::PanicShutdown("PMM INITIALIZATION FAILED (UNKNOWN REASON)\n\r");
             }
         }
     }
 
     static inline void SetupVirtualMemory() {
         if (VirtualMemory::Setup() != VirtualMemory::StatusCode::SUCCESS) {
-            Panic::PanicShutdown(rtServices, "VMM INITIALIZATION FAILED\n\r");
+            Panic::PanicShutdown("VMM INITIALIZATION FAILED\n\r");
         }
     }
 
     static inline void SetupHeap() {
         if (!Heap::Create()) {
-            Panic::PanicShutdown(rtServices, "KERNEL HEAP CREATION FAILED\n\r");
+            Panic::PanicShutdown("KERNEL HEAP CREATION FAILED\n\r");
         }
     }
 
@@ -70,17 +70,61 @@ namespace {
         VFS* vfs = static_cast<VFS*>(Heap::Allocate(sizeof(VFS)));
 
         if (vfs == nullptr) {
-            Panic::PanicShutdown(rtServices, "VFS MEMORY ALLOCATION FAILED\n\r");
+            Panic::PanicShutdown("VFS MEMORY ALLOCATION FAILED\n\r");
         }
         else if (!VFS::Construct(vfs)) {
-            Panic::PanicShutdown(rtServices, "VFS INITIALIZATION FAILED\n\r");
+            Panic::PanicShutdown("VFS INITIALIZATION FAILED\n\r");
         }
 
         return vfs;
     }
+
+    static inline void SetupPS2Keyboard() {
+        uint32_t status = 0;
+
+        Log::puts("[PS/2] Initializing PS/2 platform...\n\r");
+
+        APIC::MaskIRQ(Devices::PS2::PS2_PORT1_ISA_IRQ_VECTOR);
+
+        if ((status = Devices::PS2::InitializePS2Controller()) != 0) {
+            Log::puts("[PS/2] Controller initialization failed\n\r");
+            return;
+        }
+
+        Log::puts("[PS/2] Controller initialized\n\r");
+
+        if ((status = Devices::PS2::IdentifyPS2Port1()) > 0xFFFF) {
+            Log::puts("[PS/2] Identify failed for device on port 1\n\r");
+            return;
+        }
+
+        auto statusCode = Devices::PS2::InitializeKeyboard();
+
+        if (statusCode != Devices::PS2::StatusCode::SUCCESS) {
+            Log::puts("[PS/2] Keyboard initialization failed\n\r");
+            Log::puts("[PS/2] No keyboard input will be provided until a USB keyboard is connected\n\r");
+            return;
+        }
+
+        Interrupts::RegisterIRQ(0x40, &Devices::PS2::PS2_IRQ1_Handler, false);
+
+        APIC::SetupIRQ(Devices::PS2::PS2_PORT1_ISA_IRQ_VECTOR, {
+            .InterruptVector = 0x40,
+            .Delivery = APIC::IRQDeliveryMode::FIXED,
+            .DestinationMode = APIC::IRQDestinationMode::Logical,
+            .Polarity = APIC::IRQPolarity::ACTIVE_HIGH,
+            .Trigger = APIC::IRQTrigger::EDGE,
+            .Masked = false,
+            .Destination = APIC::GetLAPICLogicalID()
+        });
+
+        Log::puts("[PS/2] Keyboard IRQ configured\n\r");
+        Log::puts("[PS/2] Keyboard input enabled\n\r");
+        Log::puts("[PS/2] Initialization done\n\r");
+    }
 }
 
-#include <new>
+#include <cpuid.h>
 
 LEGACY_EXPORT void KernelEntry() {
     __asm__ volatile("cli");
@@ -104,6 +148,16 @@ LEGACY_EXPORT void KernelEntry() {
 
     VFS* vfs = SetupVFS();
     Log::puts("VFS Initialized\n\r");
+
+    ACPI::Initialize();
+
+    APIC::Initialize();
+
+    APIC::SetupLocalAPIC();
+
+    SetupPS2Keyboard();
+
+    __asm__ volatile("sti");
 
     while (1) {
         __asm__ volatile("hlt");
