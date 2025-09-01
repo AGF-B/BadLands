9b961d22ca27dc4c370171cc4f31ba2f221d975e
AProgrammablePhoenix <alexgf27550@gmail.com>
2025-09-01 20:55:13 +0200

    Add a build option to only build the kernel

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 if [[ $1 == "--clean" ]]; then
     rm -rf build/*
     echo "Done"
+elif [[ $1 == "--kernel" ]]; then
+    source build_kernel.sh $@ &
+    wait
+
+    cd build
+    mv kernel/kernel.img kernel.img
 else
     source build_loader.sh $@ &
     source build_kernel.sh $@ &
