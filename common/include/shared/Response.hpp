270d6add2be85e601661856e47a291c311d7e083
AProgrammablePhoenix <alexgf27550@gmail.com>
2025-11-23 23:31:03 +0100

    Add common template definition for ErrorResponse or Optionals

---
+++ common/include/shared/Response.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+template<class ErrT, typename V>
+class Response {
+public:
+    explicit inline Response(ErrT error) : isError{true}, error{error} {}
+    explicit inline Response(V value) : isError{false}, value{value} {}
+
+    inline bool CheckError() const {
+        return isError;
+    }
+
+    inline ErrT GetError() const {
+        return error;
+    }
+
+    inline V GetValue() const {
+        return value;
+    }
+
+private:
+    bool isError;
+    [[maybe_unused]] ErrT error;
+    [[maybe_unused]] V value;
+};
+
+template<typename T>
+class Optional {
+public:
+    explicit inline Optional() : hasValue{false} {}
+    explicit inline Optional(T value) : hasValue{true}, value{value} {}
+
+    inline bool HasValue() const {
+        return hasValue;
+    }
+
+    inline T GetValue() const {
+        return value;
+    }
+
+private:
+    bool hasValue;
+    [[maybe_unused]] T value;
+};
