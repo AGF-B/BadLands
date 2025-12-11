363b727dd7858cf2cef37eb3b31dc830919ce50e
AGF-B <alexgf27550@gmail.com>
2025-12-11 13:44:22 +0100

    Add simulation files

---
+++ simulation/qemusim.sh
@@ -0,0 +1,17 @@
+qemu-system-x86_64 \
+-drive file=disk.img,format=raw,media=disk \
+-drive if=pflash,format=raw,unit=0,file=OVMF_CODE-pure-efi.fd \
+-drive if=pflash,format=raw,unit=1,file=OVMF_VARS-pure-efi.fd \
+-m 256M \
+-display sdl \
+-vga std \
+-machine q35 \
+-cpu max,host-phys-bits=on,+avx,enforce \
+-device nec-usb-xhci \
+-device usb-kbd \
+-rtc base=localtime \
+-net none \
+-monitor telnet:127.0.0.1:7777,server,nowait \
+-d int,cpu_reset \
+-D qemu.log \
+-no-reboot
