1242bc6b8e1f8b9a7a04ceda75644f916749d5c6
AGF-B <alexgf27550@gmail.com>
2026-01-19 15:49:29 +0100

    Make the keyboard multiplexer less prone to deadlocks because of read and write operations happening concurrently

--- kernel/src/devices/KeyboardDispatcher/Multiplexer.cpp
+++ kernel/src/devices/KeyboardDispatcher/Multiplexer.cpp
@@ -4,6 +4,7 @@
 
 #include <shared/Lock.hpp>
 #include <shared/LockGuard.hpp>
+#include <shared/SimpleAtomic.hpp>
 
 #include <devices/KeyboardDispatcher/Keypacket.hpp>
 #include <devices/KeyboardDispatcher/Multiplexer.hpp>
@@ -28,62 +29,65 @@ namespace {
 		static constexpr size_t PACKET_SIZE = sizeof(BasicKeyPacket);
 		static constexpr size_t CAPACITY = BUFFER_SIZE / PACKET_SIZE;
 		
-		size_t available_packets;
 		uint8_t* buffer;
 		size_t location;
 
-		Utils::Lock lock;
+		Utils::Lock write_lock;
+		Utils::Lock read_lock;
+		Utils::SimpleAtomic<size_t> available_packets_count;
 
 	public:
-		GenericKeyboardBuffer(uint8_t* buffer) : FS::File(&keyboard_owner), available_packets{0}, buffer {buffer}, location{0} {
+		GenericKeyboardBuffer(uint8_t* buffer) : FS::File(&keyboard_owner), buffer {buffer}, location{0}, available_packets_count{0} {
 			static_assert(BUFFER_SIZE % PACKET_SIZE == 0);
 		}
 
 		virtual FS::Response<size_t> Read([[maybe_unused]] size_t offset, size_t count, uint8_t* buffer) final {
-			Utils::LockGuard _{lock};
-
-			if (count % PACKET_SIZE != 0) {
-				return FS::Response<size_t>(FS::Status::INVALID_PARAMETER);
-			}
-
-			size_t packets = count / PACKET_SIZE;
+			Utils::LockGuard _{read_lock};
+			
+			const size_t available_packets = available_packets_count;
 
 			if (available_packets == 0) {
 				return FS::Response<size_t>(0);
 			}
 
-			size_t read_packets = packets > available_packets ? available_packets : packets;
+			if (count % PACKET_SIZE != 0) {
+				return FS::Response<size_t>(FS::Status::INVALID_PARAMETER);
+			}
+
+			const size_t packets = count / PACKET_SIZE;
+			const size_t read_packets = packets > available_packets ? available_packets : packets;
 
 			for (size_t i = 0; i < read_packets; ++i, location = (location + 1) % CAPACITY, buffer += PACKET_SIZE) {
 				Utils::memcpy(buffer, this->buffer + location * PACKET_SIZE, PACKET_SIZE);
 			}
 
-			available_packets -= read_packets;
+			available_packets_count -= read_packets;
 
 			return FS::Response(read_packets * PACKET_SIZE);
 		}
 
 		virtual FS::Response<size_t> Write([[maybe_unused]] size_t offset, size_t count, const uint8_t* buffer) final {
-			Utils::LockGuard _{lock};
+			Utils::LockGuard _{write_lock};
 
 			if (count % PACKET_SIZE != 0) {
 				return FS::Response<size_t>(FS::Status::INVALID_PARAMETER);
 			}
 
-			size_t remaining_space = CAPACITY - available_packets;
+			const size_t available_packets = available_packets_count;
+			const size_t remaining_space = CAPACITY - available_packets;
 
 			if (remaining_space == 0) {
 				return FS::Response<size_t>(0);
 			}
 
-			size_t packets = count / PACKET_SIZE;
-			size_t written_packets = packets > remaining_space ? remaining_space : packets;
+			const size_t packets = count / PACKET_SIZE;
+			const size_t written_packets = packets > remaining_space ? remaining_space : packets;
 
 			for (size_t i = 0; i < written_packets; ++i, buffer += PACKET_SIZE) {
 				Utils::memcpy(this->buffer + ((location + available_packets + i) % CAPACITY) * PACKET_SIZE, buffer, PACKET_SIZE);
 			}
 
-			available_packets += written_packets;
+			available_packets_count += written_packets;
 
 			return FS::Response(written_packets * PACKET_SIZE);
 		}
