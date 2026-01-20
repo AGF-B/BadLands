9338e98c3503184e4126acc54d88d1a853e85c7c
AGF-B <alexgf27550@gmail.com>
2026-01-20 16:12:03 +0100

    License repository

--- build_loader.sh
+++ build_loader.sh
@@ -1,3 +1,17 @@
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# Copyright (C) 2026 Alexandre Boissiere
+# This file is part of the BadLands operating system.
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, version 3.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <https://www.gnu.org/licenses/>. 
+
 mkdir -p build
 cd build
 cmake -D CMAKE_C_COMPILER="x86_64-w64-mingw32-gcc" -D CMAKE_CXX_COMPILER="x86_64-w64-mingw32-g++" ../bootloader/
