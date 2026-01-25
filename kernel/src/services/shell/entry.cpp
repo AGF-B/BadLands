175f9cf065ad49e4d712a8b283f0424afed0bbae
AGF-B <alexgf27550@gmail.com>
2026-01-25 14:08:50 +0100

    Keep moving shell components into the input buffer wrapper

--- kernel/src/services/shell/entry.cpp
+++ kernel/src/services/shell/entry.cpp
@@ -46,11 +46,29 @@ namespace {
         size_t length;
     };
 
+    typedef void (*ExecuteCallback)(const CommandString&);
+    typedef Devices::KeyboardDispatcher::VirtualKeyPacket KeyPacket;
+
     class InputBuffer final {
     private:
         char* buffer    = nullptr;
         size_t capacity = 0;
         size_t position = 0;
+
+        ExecuteCallback executeCallback = nullptr;
+
+        void AppendChar(char c) {
+            if (!IsOverflowing()) {
+                buffer[position++] = c;
+            }
+        }
+
+        CommandString GetCommandString() const {
+            return CommandString {
+                .command = buffer,
+                .length = position
+            };
+        }
         
     public:
         explicit InputBuffer(size_t size) : capacity{size} {
@@ -77,23 +95,102 @@ namespace {
             return position >= capacity - 1;
         }
 
-        void AppendChar(char c) {
-            if (!IsOverflowing()) {
-                buffer[position++] = c;
-            }
-        }
-
         void OnBackspace() {
             if (position > 0) {
                 buffer[--position] = '\0';
             }
         }
 
-        CommandString GetCommandString() const {
-            return CommandString {
-                .command = buffer,
-                .length = position
-            };
+        void SetExecuteCallback(const ExecuteCallback& callback) {
+            executeCallback = callback;
+        }
+
+        void OnKeyEvent(const KeyPacket& pkt) {
+            if (pkt.flags & Devices::KeyboardDispatcher::FLAG_KEY_PRESSED) {
+                bool shift = (pkt.flags & Devices::KeyboardDispatcher::FLAG_LEFT_SHIFT) ||
+                                (pkt.flags & Devices::KeyboardDispatcher::FLAG_RIGHT_SHIFT);
+
+                bool control = (pkt.flags & Devices::KeyboardDispatcher::FLAG_LEFT_CONTROL) ||
+                                (pkt.flags & Devices::KeyboardDispatcher::FLAG_RIGHT_CONTROL);
+
+                bool alt = (pkt.flags & Devices::KeyboardDispatcher::FLAG_LEFT_ALT) ||
+                            (pkt.flags & Devices::KeyboardDispatcher::FLAG_RIGHT_ALT);
+                
+                if (control || alt) {
+                    return;
+                }
+
+                char shift_offset = shift ? 'A' - 'a' : 0;
+
+                char c = '\0';
+
+                switch (pkt.keycode) {
+                    case VK_A: c = 'a' + shift_offset; break;
+                    case VK_B: c = 'b' + shift_offset; break;
+                    case VK_C: c = 'c' + shift_offset; break;
+                    case VK_D: c = 'd' + shift_offset; break;
+                    case VK_E: c = 'e' + shift_offset; break;
+                    case VK_F: c = 'f' + shift_offset; break;
+                    case VK_G: c = 'g' + shift_offset; break;
+                    case VK_H: c = 'h' + shift_offset; break;
+                    case VK_I: c = 'i' + shift_offset; break;
+                    case VK_J: c = 'j' + shift_offset; break;
+                    case VK_K: c = 'k' + shift_offset; break;
+                    case VK_L: c = 'l' + shift_offset; break;
+                    case VK_M: c = 'm' + shift_offset; break;
+                    case VK_N: c = 'n' + shift_offset; break;
+                    case VK_O: c = 'o' + shift_offset; break;
+                    case VK_P: c = 'p' + shift_offset; break;
+                    case VK_Q: c = 'q' + shift_offset; break;
+                    case VK_R: c = 'r' + shift_offset; break;
+                    case VK_S: c = 's' + shift_offset; break;
+                    case VK_T: c = 't' + shift_offset; break;
+                    case VK_U: c = 'u' + shift_offset; break;
+                    case VK_V: c = 'v' + shift_offset; break;
+                    case VK_W: c = 'w' + shift_offset; break;
+                    case VK_X: c = 'x' + shift_offset; break;
+                    case VK_Y: c = 'y' + shift_offset; break;
+                    case VK_Z: c = 'z' + shift_offset; break;
+                    case VK_0: c = shift ? ')' : '0'; break;
+                    case VK_1: c = shift ? '!' : '1'; break;
+                    case VK_2: c = shift ? '@' : '2'; break;
+                    case VK_3: c = shift ? '#' : '3'; break;
+                    case VK_4: c = shift ? '$' : '4'; break;
+                    case VK_5: c = shift ? '%' : '5'; break;
+                    case VK_6: c = shift ? '^' : '6'; break;
+                    case VK_7: c = shift ? '&' : '7'; break;
+                    case VK_8: c = shift ? '*' : '8'; break;
+                    case VK_9: c = shift ? '(' : '9'; break;
+                    case VK_SPACE: c = ' '; break;
+                    case VK_RETURN: c = '\n'; break;
+                    case VK_BACK:
+                        OnBackspace();
+                        Log::putcSafe('\b');
+                        break;
+                    default: break;
+                }
+
+                if (c != '\0' && c != '\n') {
+                    AppendChar(c);
+                    Log::putcSafe(c);
+                }
+                else if (c == '\n') {
+                    Log::putsSafe("\n\r");
+
+                    if (IsOverflowing()) {
+                        Log::putsSafe("[SHELL] Command too long.\n\r");
+                    }
+                    else {
+                        if (executeCallback != nullptr) {
+                            executeCallback(GetCommandString());
+                        }
+                    }
+
+                    Clear();
+
+                    Log::putsSafe("> ");
+                }
+            }
         }
 
         void Clear() {
@@ -103,6 +200,19 @@ namespace {
             }
         }
     };
+
+    void OnExecute(const CommandString& cmd) {
+        const char* const cmd_string = cmd.command;
+
+        if (cmd.length == 5 && Utils::memcmp(cmd_string, "clear", 5) == 0) {
+            Log::clear();
+        }
+        else {
+            Log::putsSafe("[SHELL] Unknown command: ");
+            Log::putsSafe(cmd_string);
+            Log::putsSafe("\n\r");
+        }
+    }
 }
 
 namespace Services {
@@ -116,20 +226,22 @@ namespace Services {
                 .Name = keyboardBufferPath
             };
 
-            auto response = Kernel::Exports.vfs->Open(keyboardBufferEntry);
+            const auto response = Kernel::Exports.vfs->Open(keyboardBufferEntry);
 
             if (response.CheckError()) {
                 Log::printfSafe("Error code: %d\n\r", static_cast<int>(response.GetError()));
                 Panic::PanicShutdown("(SHELL) COULD NOT OPEN KEYBOARD BUFFER\n\r");
             }
 
-            auto keyboardBuffer = response.GetValue();
+            const auto keyboardBuffer = response.GetValue();
 
             InputBuffer inputBuffer(BUFFER_SIZE);
 
             if (!inputBuffer.IsValid()) {
                 Panic::PanicShutdown("(SHELL) COULD NOT START KERNEL SHELL\n\r");
             }
+
+            inputBuffer.SetExecuteCallback(OnExecute);
 
             Log::putsSafe("[SHELL] Kernel shell initialized\n\r");
             Log::putsSafe("> ");
@@ -139,109 +251,14 @@ namespace Services {
 
                 Devices::KeyboardDispatcher::BasicKeyPacket packet;
 
-                auto v = keyboardBuffer->Read(0, sizeof(packet), reinterpret_cast<uint8_t*>(&packet));
+                const auto v = keyboardBuffer->Read(0, sizeof(packet), reinterpret_cast<uint8_t*>(&packet));
 
                 if (v.CheckError()) {
                     Log::putsSafe("Error reading\n\r");
                 }
                 else {
                     if (v.GetValue() > 0) {
-                        auto vpkt = Devices::KeyboardDispatcher::GetVirtualKeyPacket(packet);
-
-                        if (vpkt.flags & Devices::KeyboardDispatcher::FLAG_KEY_PRESSED) {
-                            bool shift = (vpkt.flags & Devices::KeyboardDispatcher::FLAG_LEFT_SHIFT) ||
-                                         (vpkt.flags & Devices::KeyboardDispatcher::FLAG_RIGHT_SHIFT);
-
-                            bool control = (vpkt.flags & Devices::KeyboardDispatcher::FLAG_LEFT_CONTROL) ||
-                                           (vpkt.flags & Devices::KeyboardDispatcher::FLAG_RIGHT_CONTROL);
-
-                            bool alt = (vpkt.flags & Devices::KeyboardDispatcher::FLAG_LEFT_ALT) ||
-                                       (vpkt.flags & Devices::KeyboardDispatcher::FLAG_RIGHT_ALT);
-                            
-                            if (control || alt) {
-                                continue;
-                            }
-
-                            char shift_offset = shift ? 'A' - 'a' : 0;
-
-                            char c = '\0';
-
-                            switch (vpkt.keycode) {
-                                case VK_A: c = 'a' + shift_offset; break;
-                                case VK_B: c = 'b' + shift_offset; break;
-                                case VK_C: c = 'c' + shift_offset; break;
-                                case VK_D: c = 'd' + shift_offset; break;
-                                case VK_E: c = 'e' + shift_offset; break;
-                                case VK_F: c = 'f' + shift_offset; break;
-                                case VK_G: c = 'g' + shift_offset; break;
-                                case VK_H: c = 'h' + shift_offset; break;
-                                case VK_I: c = 'i' + shift_offset; break;
-                                case VK_J: c = 'j' + shift_offset; break;
-                                case VK_K: c = 'k' + shift_offset; break;
-                                case VK_L: c = 'l' + shift_offset; break;
-                                case VK_M: c = 'm' + shift_offset; break;
-                                case VK_N: c = 'n' + shift_offset; break;
-                                case VK_O: c = 'o' + shift_offset; break;
-                                case VK_P: c = 'p' + shift_offset; break;
-                                case VK_Q: c = 'q' + shift_offset; break;
-                                case VK_R: c = 'r' + shift_offset; break;
-                                case VK_S: c = 's' + shift_offset; break;
-                                case VK_T: c = 't' + shift_offset; break;
-                                case VK_U: c = 'u' + shift_offset; break;
-                                case VK_V: c = 'v' + shift_offset; break;
-                                case VK_W: c = 'w' + shift_offset; break;
-                                case VK_X: c = 'x' + shift_offset; break;
-                                case VK_Y: c = 'y' + shift_offset; break;
-                                case VK_Z: c = 'z' + shift_offset; break;
-                                case VK_0: c = shift ? ')' : '0'; break;
-                                case VK_1: c = shift ? '!' : '1'; break;
-                                case VK_2: c = shift ? '@' : '2'; break;
-                                case VK_3: c = shift ? '#' : '3'; break;
-                                case VK_4: c = shift ? '$' : '4'; break;
-                                case VK_5: c = shift ? '%' : '5'; break;
-                                case VK_6: c = shift ? '^' : '6'; break;
-                                case VK_7: c = shift ? '&' : '7'; break;
-                                case VK_8: c = shift ? '*' : '8'; break;
-                                case VK_9: c = shift ? '(' : '9'; break;
-                                case VK_SPACE: c = ' '; break;
-                                case VK_RETURN: c = '\n'; break;
-                                case VK_BACK:
-                                    inputBuffer.OnBackspace();
-                                    Log::putcSafe('\b');
-                                    break;
-                                default: break;
-                            }
-
-                            if (c != '\0' && c != '\n') {
-                                inputBuffer.AppendChar(c);
-                                Log::putcSafe(c);
-                            }
-                            else if (c == '\n') {
-                                Log::putsSafe("\n\r");
-
-                                if (inputBuffer.IsOverflowing()) {
-                                    Log::putsSafe("[SHELL] Command too long.\n\r");
-                                }
-                                else {
-                                    const auto command = inputBuffer.GetCommandString();
-                                    const char* const cmd_string = command.command;
-
-                                    if (command.length == 5 && Utils::memcmp(cmd_string, "clear", 5) == 0) {
-                                        Log::clear();
-                                    }
-                                    else {
-                                        Log::printfSafe("Command length: %u\n\r", static_cast<unsigned>(command.length));
-                                        Log::putsSafe("[SHELL] Unknown command: ");
-                                        Log::putsSafe(cmd_string);
-                                        Log::putsSafe("\n\r");
-                                    }
-                                }
-
-                                inputBuffer.Clear();
-
-                                Log::putsSafe("> ");
-                            }
-                        }
+                        inputBuffer.OnKeyEvent(Devices::KeyboardDispatcher::GetVirtualKeyPacket(packet));
                     }
                 }
             }
