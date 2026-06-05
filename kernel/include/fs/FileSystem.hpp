cbeef5f6c81019801eb835347d2457ab339d19e8
AGF-B <alexgf27550@gmail.com>
2026-06-05 16:41:42 +0200

    Add scaffold for FileSystem class

---
+++ kernel/include/fs/FileSystem.hpp
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// Copyright (C) 2026 Alexandre Boissiere
+// This file is part of the BadLands operating system.
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the
+// GNU General Public License as published by the Free Software Foundation, version 3.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program.
+// If not, see <https://www.gnu.org/licenses/>. 
+
+#pragma once
+
+#include <fs/IFNode.hpp>
+
+#include <kern/memory.hpp>
+
+class FileSystem {
+public:
+    virtual kern::shared_ptr<FS::IFNode> GetRoot() = 0;
+
+    static kern::shared_ptr<FileSystem> AutoDetect(const kern::shared_ptr<FS::IFNode>& device);
+};
