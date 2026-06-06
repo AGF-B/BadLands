a2039829522c44007e5fcede6b0fb67509e6dec1
AGF-B <alexgf27550@gmail.com>
2026-06-06 23:01:02 +0200

    Change signature of FileSystem::AutoDetect to return unique_ptr instead of shared_ptr

--- kernel/src/fs/FileSystem.cpp
+++ kernel/src/fs/FileSystem.cpp
@@ -17,6 +17,6 @@
 
 #include <kern/memory.hpp>
 
-kern::shared_ptr<FileSystem> FileSystem::AutoDetect(const kern::shared_ptr<FS::IFNode>& device) {
+kern::unique_ptr<FileSystem> FileSystem::AutoDetect(const kern::shared_ptr<FS::IFNode>& device) {
     return {};
 }
